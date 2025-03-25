2c188954c5174d9079570240e6e7c60343830ca7
RITIKA-1104 <ritikapandey1104@gmail.com>
2025-03-25 18:50:56 +0530

    frontend;contact page completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
+import relatedDoctors from '../components/relatedDoctors'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <SpecialityMenu/>
         <TopDoctors/>
         <Banner/>
+        <relatedDoctors/>
     </div>
   )
 }
